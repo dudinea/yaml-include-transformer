bf3ea693f1375475bf91e65ff6579d8f9aa13d02
Eugene Doudine <eugene.d@trekflow.com>
2022-07-16 20:26:09 +0300

    refactor: separate transformer in separate package

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	b64 "encoding/base64"
-	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
-	"reflect"
-	"strings"
 
 	"github.com/dudinea/kustomize-field-include/pkg/config"
 	"github.com/dudinea/kustomize-field-include/pkg/kustomize"
-	"gopkg.in/yaml.v3"
+	"github.com/dudinea/kustomize-field-include/pkg/transform"
 )
-
-var header = []byte("---\n")
-
-// Include suffixes
-const TEXTFILE = "!textfile"
-const BASE64FILE = "!base64file"
 
 func main() {
 	var err error
@@ -33,9 +21,9 @@ func main() {
 	if conf.ExecInstall {
 		err = kustomize.PluginInstall()
 		if nil != err {
-			errexit(2, "Kustomize exec plugin installation failed: %v", err)
+			transform.Errexit(2, "Kustomize exec plugin installation failed: %v", err)
 		} else {
-			errexit(0, "Kustomize exec plugin Installation complete")
+			transform.Errexit(0, "Kustomize exec plugin Installation complete")
 		}
 	}
 	if conf.PluginConf {
@@ -44,141 +32,5 @@ func main() {
 	}
 
 	reader := os.Stdin
-	err = nil
-
-	decoder := yaml.NewDecoder(reader)
-	var m interface{}
-
-	for err == nil {
-		err = decoder.Decode(&m)
-		if nil == err {
-			err = processAny(m)
-			if nil != err {
-				errexit(5, "Failed to process yaml: %v", err.Error())
-			}
-			outBytes, err := yaml.Marshal(m)
-			if nil != err {
-				errexit(5, "Failed to convert to yaml: %v", err.Error())
-			}
-			writeBytes(&header)
-			writeBytes(&outBytes)
-		}
-	}
-
-	if err != io.EOF {
-		errexit(3, "Error reading input stream: %v", err.Error())
-	}
-
-}
-
-// return include type and original key
-func isInclude(k string) (include_type string, new_key string) {
-	//fmt.Fprintf(os.Stderr, "%v: isInclude: %v\n", os.Args[0], k)
-	if strings.HasSuffix(k, TEXTFILE) {
-		return TEXTFILE, k[0 : len(k)-len(TEXTFILE)]
-	} else if strings.HasSuffix(k, BASE64FILE) {
-		return BASE64FILE, k[0 : len(k)-len(BASE64FILE)]
-	}
-	return "", k
-}
-
-// FIXME: security: disallow absolute paths and other tricks
-func readFile(path string) ([]byte, error) {
-	bytes, err := ioutil.ReadFile(path)
-	if nil != err {
-		return nil, err
-	}
-	return bytes, nil
-}
-
-func includeTextfile(v interface{}) (string, error) {
-	data, err := includeFile(v)
-	if nil != err {
-		return "", err
-	}
-	return string(data), err
-}
-
-func includeFile(v interface{}) ([]byte, error) {
-	var data []byte
-	var err error
-	switch v.(type) {
-	case string:
-		data, err = readFile(v.(string))
-	default:
-		err = fmt.Errorf("Invalid value for include specification: %v", reflect.TypeOf(v))
-	}
-	if nil != err {
-		return nil, err
-	}
-	return data, err
-}
-
-func include(incl_type string, k string, v interface{}) (interface{}, error) {
-	switch incl_type {
-	case TEXTFILE:
-		return includeTextfile(v)
-	case BASE64FILE:
-		data, err := includeFile(v)
-		if nil != err {
-			return "", err
-		}
-		encoded := make([]byte, b64.StdEncoding.EncodedLen(len(data)))
-		b64.StdEncoding.Encode(encoded, data)
-		return string(encoded), nil
-	default:
-		return v, fmt.Errorf("Internal error: invalid include type %s", incl_type)
-	}
-}
-
-func processMap(m map[string]interface{}) error {
-	for k, v := range m {
-		//fmt.Fprintf(os.Stderr, "%v: process map: %v: %v\n", os.Args[0], k, v)
-		incl_type, new_key := isInclude(k)
-		if incl_type != "" {
-			//fmt.Fprintf(os.Stderr, "%v: include: %v: %v\n", os.Args[0], k, v)
-			newval, err := include(incl_type, k, v)
-			if err != nil {
-				return err
-			}
-			m[new_key] = newval
-			delete(m, k)
-		} else {
-			processAny(v)
-		}
-	}
-	return nil
-}
-
-func processArray(a []interface{}) error {
-	for _, k := range a {
-		err := processAny(k)
-		if nil != err {
-			return err
-		}
-	}
-	return nil
-}
-
-func processAny(data interface{}) error {
-	switch data.(type) {
-	case map[string]interface{}:
-		return processMap(data.(map[string]interface{}))
-	case []interface{}:
-		return processArray(data.([]interface{}))
-	}
-	return nil
-}
-
-func errexit(code int, msg string, args ...interface{}) {
-	fmt.Fprint(os.Stderr, os.Args[0])
-	fmt.Fprintf(os.Stderr, msg+"\n", args...)
-	os.Exit(code)
-}
-
-func writeBytes(bytes *[]byte) {
-	_, err := os.Stdout.Write(*bytes)
-	if nil != err {
-		errexit(5, "Failed to write output: %v", err.Error())
-	}
+	transform.Transform(reader)
 }
