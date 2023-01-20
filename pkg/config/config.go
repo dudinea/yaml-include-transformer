d4e794e1564fc567b1058e9e461ba8abffc4bee9
Evgeni Doudine <dudinea@gmail.com>
2023-01-20 22:40:32 +0200

    doc; fix help message

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Config struct {
+	Debug       bool
 	PrintUsage  bool
 	ExecInstall bool
 	PluginConf  bool
@@ -41,10 +42,12 @@ func ReadArgs(args []string) (error, Config) {
 	fs.BoolVar(&conf.Updir, "updir", false, "Allow specifying .. in file paths")
 	fs.BoolVar(&conf.Links, "l", false, "Allow following symlinks in file paths")
 	fs.BoolVar(&conf.Links, "links", false, "Allow following symlinks in file paths")
-	fs.BoolVar(&conf.Abs, "a", false, "Allow following symlinks in file paths")
+	fs.BoolVar(&conf.Abs, "a", false, "Allow absolute file paths")
 	fs.BoolVar(&conf.Abs, "abs", false, "Allow absolute file paths")
-	fs.BoolVar(&conf.Version, "v", false, "Allow following symlinks in file paths")
-	fs.BoolVar(&conf.Version, "version", false, "Allow absolute file paths")
+	fs.BoolVar(&conf.Version, "v", false, "Print program version")
+	fs.BoolVar(&conf.Version, "version", false, "Print program version")
+	fs.BoolVar(&conf.Version, "d", false, "Print debug messages on stderr")
+	fs.BoolVar(&conf.Version, "debug", false, "Print debug messages on stderr")
 	err := fs.Parse(args)
 	return err, conf
 }
@@ -71,7 +74,10 @@ const usagestr = "\nUsage: \n" +
 	"  -l --links          Allow following symlinks in file paths\n" +
 	"  -a --abs            Allow absolute paths in file paths\n" +
 	"  -v --version        Print program version\n" +
+	"  -d --debug          Print debug messages on stderr\n" +
 	"\n" +
 	"Supported YAML include directives:\n" +
 	"  foo!textfile: file.txt    -- include file.txt as text field\n" +
-	"  bar:base64file: file.bin  -- include file.bin as base64 text\n"
+	"  foo!base64file: file.bin  -- include file.bin as base64 text\n" +
+	"  foo!jsonfile: file.json   -- deserialize and include file.json\n" +
+	"  foo!yamlfile: file.yaml   -- deserialize and include file.yaml\n"
